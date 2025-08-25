452fdfb10c226535d0c334f53368381324ce4b25
ikram <ikramayachi.532@gmail.com>
2025-08-25 09:48:12 +0100

    Add URL Shortener microservice

--- index.js
+++ index.js
@@ -1,30 +1,70 @@
 // index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const urlParser = require('url');
 
-// http://expressjs.com/en/starter/static-files.html
+// Middleware
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// Page d'accueil
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
+// Exemple fourni
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// listen for requests :)
+// -----------------------------
+// URL Shortener Microservice
+// -----------------------------
+let urlDatabase = {};
+let counter = 1;
+
+// POST pour raccourcir l’URL
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+  const parsedUrl = urlParser.parse(originalUrl);
+
+  if (!parsedUrl.protocol || !parsedUrl.hostname) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  dns.lookup(parsedUrl.hostname, (err) => {
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    }
+
+    const shortUrl = counter++;
+    urlDatabase[shortUrl] = originalUrl;
+
+    res.json({
+      original_url: originalUrl,
+      short_url: shortUrl,
+    });
+  });
+});
+
+// GET pour rediriger
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = req.params.short_url;
+  const originalUrl = urlDatabase[shortUrl];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'No short URL found' });
+  }
+});
+
+// Listen
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
